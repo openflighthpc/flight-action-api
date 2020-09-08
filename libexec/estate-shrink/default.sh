2d4ae3e63372339ef10c29e8843efaacf5f98f6e
William McCumsite <william.mccumstie@gmail.com>
2020-09-08 09:44:55 +0100

    Remember to pass estate-shrink args through

--- libexec/estate-shrink/default.sh
+++ libexec/estate-shrink/default.sh
@@ -30,4 +30,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Runs the estate-grow script with the 'Shrink' type
 __flight_ESTATE_action='Shrink'
-bash "${SCRIPT_ROOT:-.}"/estate-grow/default.sh
+bash "${SCRIPT_ROOT:-.}"/estate-grow/default.sh "$@"
