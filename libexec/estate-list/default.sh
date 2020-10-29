e66d6d7712a67dda8ff89468c5610fd26ff96660
William McCumsite <william.mccumstie@gmail.com>
2020-10-29 13:21:50 +0000

    Add a basic estate-show/estate-list command

---
+++ libexec/estate-list/default.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+LIBEXEC="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+
+# Get the last line from the show output
+bash "$LIBEXEC"/estate-show/default.sh "$@" | tail -n1
