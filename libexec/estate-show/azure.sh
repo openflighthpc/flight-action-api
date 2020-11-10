b997acc4a21f614be3373a3e2f42c44f917cabb0
William McCumsite <william.mccumstie@gmail.com>
2020-11-10 14:00:07 +0000

    Implement the basic estate-show command for azure

---
+++ libexec/estate-show/azure.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#==============================================================================
+# Copyright (C) 2020-present Alces Flight Ltd.
+#
+# This file is part of Flight Action API.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# Flight Action API is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Action API. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on Flight Action API, please visit:
+# https://github.com/openflighthpc/flight-action-api
+#===============================================================================
+
+# Error if the resource group has not been given
+if [[ -z "${azure_resource_group}" ]]; then
+    echo "The azure_resource_group for node '$name' has not been set!" >&2
+    exit 1
+fi
+
+# Default the azure_name to be the same as name
+echo "${azure_name:=$name}" >/dev/null
+
+# Source the resource mappings
+source "${SCRIPT_ROOT:-.}"/helpers/azure-machine-type-definitions.sh
+
+azure_instance_type=$(
+  az vm get-instance-view \
+    --resource-group  "$azure_resource_group" \
+    --name "$azure_name" \
+    --query hardwareProfile.vmSize
+)
+
+exit_code=$?
+
+if [ ${exit_code} -eq 0 ] ; then
+    # Remove the surrounding quotes.
+    azure_instance_type="${azure_instance_type%\"}"
+    azure_instance_type="${azure_instance_type#\"}"
+
+    machine_type="${REVERSE_MACHINE_TYPE_MAP[$azure_instance_type]}"
+    if [ "${machine_type}" == "" ]; then
+        echo "unknown (${aws_instance_type})"
+    else
+        echo "${machine_type}"
+    fi
+else
+    # Standard error from the `az` call should be enough to debug this.
+    :
+fi
+
+exit ${exit_code}
---
+++ libexec/helpers/azure-machine-type-definitions.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#==============================================================================
+# Copyright (C) 2020-present Alces Flight Ltd.
+#
+# This file is part of Flight Action API.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# Flight Action API is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Action API. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on Flight Action API, please visit:
+# https://github.com/openflighthpc/flight-action-api
+#===============================================================================
+
+# A map from generic machine types to the equivalent Azure instance type.
+#
+# NOTE: If changing this map also change the types listed in the estate
+# metadata files.
+declare -A MACHINE_TYPE_MAP
+MACHINE_TYPE_MAP=(
+  [general-small]=Standard_B2s
+  [general-medium]=Undefined_general_medium
+  [general-large]=Undefiend_general_large
+
+  [compute-small]=Undefined_compute_small
+  [compute-medium]=Undefined_compute_medium
+  [compute-large]=Undefined_compute_large
+
+  [gpu-small]=Undefined_gpu_small
+  [gpu-medium]=Undefined_gpu_medium
+  [gpu-large]=Undefined_gpu_large
+
+  [mem-small]=Undefined_mem_small
+  [mem-medium]=Undefined_mem_medium
+  [mem-large]=Undefined_mem_large
+)
+
+# An array of machine types.
+declare -a MACHINE_TYPE_NAMES
+set_type_names() {
+    local key
+    while IFS= read -rd '' key; do
+        MACHINE_TYPE_NAMES+=( "$key" )
+    done < <(printf '%s\0' "${!MACHINE_TYPE_MAP[@]}" | sort -z)
+}
+set_type_names
+unset -f set_type_names
+
+# A map from AWS EC2 instance type to generic machine type.
+declare -A REVERSE_MACHINE_TYPE_MAP
+set_reverse_map() {
+    local machine_type
+    local instance_type
+    for machine_type in "${!MACHINE_TYPE_MAP[@]}" ; do
+        instance_type="${MACHINE_TYPE_MAP[${machine_type}]}"
+        REVERSE_MACHINE_TYPE_MAP[$instance_type]="${machine_type}"
+    done
+}
+set_reverse_map
+unset -f set_reverse_map
+
+validate_machine_type() {
+    if [ "${MACHINE_TYPE_MAP[${1}]}" == "" ]; then
+        cat 1>&2 <<ERROR
+Unknown machine type ${1}.  Available machine types:
+
+$( printf '  %s\n' "${MACHINE_TYPE_NAMES[@]}" )
+ERROR
+        exit 1
+    fi
+}
