dbfa9f324db55ac2e9a7e4c724cc867d3b330bb7
William McCumsite <william.mccumstie@gmail.com>
2020-11-12 15:57:55 +0000

    Refactor validate_machine_type to support filtering
    Not all the types will be implemented on every platform. The
    non-implemented types need to be filter out from the error mesages
    
    Also azure does not support arbitrarily resizing a node to a different
    type. Instead an optional filter has been added to only allow resizing
    within a single family

--- libexec/helpers/basic-machine-type-definitions.sh
+++ libexec/helpers/basic-machine-type-definitions.sh
@@ -59,19 +59,85 @@ define_reverse_machine_type_map() {
   local instance_type
 
   for machine_type in "${!MACHINE_TYPE_MAP[@]}" ; do
-      instance_type="${MACHINE_TYPE_MAP[${machine_type}]}"
-      REVERSE_MACHINE_TYPE_MAP[$instance_type]="${machine_type}"
+    instance_type="${MACHINE_TYPE_MAP[${machine_type}]}"
+    REVERSE_MACHINE_TYPE_MAP[$instance_type]="${machine_type}"
+  done
+}
+
+# Determines the machine types with a family
+update_filtered_type_names_by_family() {
+  # Create an array named according to $1. The family is given by $2
+  local family=$1
+  local machine_type
+  FILTERED_TYPE_NAMES=()
+
+  for machine_type in "${!MACHINE_TYPE_MAP[@]}" ; do
+    if [[ "$machine_type" =~ "^${family}-" ]]; then
+      FILTERED_TYPE_NAMES+=("$machine_type")
+    fi
   done
 }
 
 # Check if the machine type is valid
 validate_machine_type() {
-    if [ "${MACHINE_TYPE_MAP[${1}]}" == "" ]; then
-        cat 1>&2 <<ERROR
-Unknown machine type ${1}.  Available machine types:
+  # Unpack args
+  local new_type="$1"
+  local family=$(echo "$2" | sed 's/-.*//')
+  local valid=0
 
-$( printf '  %s\n' "${MACHINE_TYPE_NAMES[@]}" )
-ERROR
-        exit 1
+  # Determines if the new_type is unknown
+  # NOTE: This reason maybe overridden when valid_types is generated
+  if [ -z "${MACHINE_TYPE_MAP["$new_type"]}" ]; then
+    valid=1
+  fi
+
+  # Loops through all the known machine types, doing the following:
+  # 1. Generating the valid list of types (optionally filtered by family)
+  # 2. Detects if the type is unsupported or outside the family
+  local -a valid_types
+  for machine_type in "${MACHINE_TYPE_NAMES[@]}" ; do
+    # Filters out unsupported types
+    if [ -z ${MACHINE_TYPE_MAP["$machine_type"]} ]; then
+      if [ "$new_type" == "$machine_type" ]; then
+        valid=2
+      fi
+
+    # Adds the type if it is within the family (or if the filter is off)
+    elif [[ -z "$family" || "$machine_type" =~ ^${family}- ]]; then
+      valid_types+=("$machine_type")
+
+    # Flags the new_type as invalid due to the family filter
+    elif [ "$new_type" == "$machine_type" ]; then
+      valid=3
     fi
+  done
+
+  # Processes the error messages
+  case "$valid" in
+    1)
+      cat 1>&2 <<ERROR
+Unknown machine type ${new_type}. Available machine types:
+
+$( printf '  %s\n' "${valid_types[@]}" )
+ERROR
+      exit 101
+      ;;
+    2)
+      cat 1>&2 <<ERROR
+This platform does not suport ${new_type}. Available machine types:
+
+$( printf '  %s\n' "${valid_types[@]}" )
+ERROR
+      exit 102
+      ;;
+    3)
+      cat 1>&2 <<ERROR
+Changing the machine type family is not support on this platform.
+Please select a '$family' type:
+
+$( printf '  %s\n' "${valid_types[@]}" )
+ERROR
+      exit 103
+      ;;
+  esac
 }
